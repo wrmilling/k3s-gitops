e59b5a1dbd79e7be5439f6378c74c52c35aaad09
Winston R. Milling <Winston@Milli.ng>
2021-02-22 16:40:43 -0600

    Setting up basic script and secrets for setup

--- setup/README.md
+++ setup/README.md
@@ -1,3 +1,15 @@
 # Cluster setup with k3s
 
-Setup is pending new hardware being racked in my homelab. (02-02-2021)
+## amd64 + arm64 Cluster Installation (One-Time Actions)
+
+### k3s Node Setup
+
+Check out [homelab-infrastructure/k3s](https://github.com/WRMilling/homelab-infrastructure/tree/master/k3s) for information on creating the physical/virtual cluster. Make sure you have `kubectl` setup on the machine you run these steps on and that it has access to the cluster. This requires either `$KUBECONFIG` being set properly or `~/.kube/config` being populated. 
+
+### Bootstrapping the Cluster
+
+Simply run `./bootstrap-cluster.sh` located in this directory to setup the cluster. This does the following things: 
+
+1. Installs flux2
+
+This script was originally pulled from [billimek/k8s-gitops](https://github.com/billimek/k8s-gitops/blob/master/setup/bootstrap-cluster.sh) but may have some modifications based on my needs. 
---
+++ setup/bootstrap-cluster.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+need() {
+    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+}
+
+message() {
+  echo -e "\n######################################################################"
+  echo "# $1"
+  echo "######################################################################"
+}
+
+installFlux() {
+  message "installing fluxv2"
+  flux check --pre > /dev/null
+  FLUX_PRE=$?
+  if [ $FLUX_PRE != 0 ]; then 
+    echo -e "flux prereqs not met:\n"
+    flux check --pre
+    exit 1
+  fi
+  if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is not set! Check $REPO_ROOT/setup/.env"
+    exit 1
+  fi
+  flux bootstrap github \
+    --owner=wrmilling \
+    --repository=k3s-gitops \
+    --branch master \
+    --personal
+
+  FLUX_INSTALLED=$?
+  if [ $FLUX_INSTALLED != 0 ]; then 
+    echo -e "flux did not install correctly, aborting!"
+    exit 1
+  fi
+}
+
+need "kubectl"
+need "helm"
+need "flux"
+installFlux
+
+message "all done!"
+kubectl get nodes -o=wide
