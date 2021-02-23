4f1176013ebc9f31679b10cd89a6f45ca1556da8
Winston R. Milling <Winston@Milli.ng>
2021-02-23 16:40:37 -0600

    Add cert-manager with route53 for domain

---
+++ setup/seal-secrets.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+. "$REPO_ROOT"/setup/.env
+
+seal() {
+  kubeseal --cert ${REPO_ROOT}/setup/sealed-secrets.crt -o yaml $@
+}
+
+if [ ! -f "${REPO_ROOT}/secrets/route53-api-key.yaml" ]; then
+  kubectl create secret --namespace=cert-manager generic route53-api-key --from-literal=key="${ROUTE53_KEY}" --dry-run=client -o yaml | seal > "${REPO_ROOT}"/secrets/route53-api-key.yaml
+fi
