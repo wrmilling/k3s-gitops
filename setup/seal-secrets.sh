61e20ef08262b4d50fecd4cd7d3abe61cec4226e
Winston R. Milling <Winston@Milli.ng>
2021-02-23 22:05:54 -0600

    Moving around cert-manager files

--- setup/seal-secrets.sh
+++ setup/seal-secrets.sh
@@ -7,6 +7,6 @@ seal() {
   kubeseal --cert ${REPO_ROOT}/setup/sealed-secrets.crt -o yaml $@
 }
 
-if [ ! -f "${REPO_ROOT}/secrets/route53-api-key.yaml" ]; then
-  kubectl create secret --namespace=cert-manager generic route53-api-key --from-literal=key="${ROUTE53_KEY}" --dry-run=client -o yaml | seal > "${REPO_ROOT}"/secrets/route53-api-key.yaml
+if [ ! -f "${REPO_ROOT}/cert-manager/route53-api-key.yaml" ]; then
+  kubectl create secret --namespace=cert-manager generic route53-api-key --from-literal=key="${ROUTE53_KEY}" --dry-run=client -o yaml | seal > "${REPO_ROOT}"/cert-manager/route53-api-key.yaml
 fi
