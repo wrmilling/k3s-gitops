a6f1b117a2a981aeacd6472acc62e2fa34620766
Winston R. Milling <Winston@Milli.ng>
2021-08-21 18:05:46 -0500

    SOPS: Migrate kube-system to SOPS

--- setup/sops-encrypt.sh
+++ setup/sops-encrypt.sh
@@ -27,13 +27,13 @@ seal 'flux-system-extra/github-alerts' 'github-api-token'
 seal 'flux-system-extra/discord-alerts' 'discord-webhook-url'
 
 # kube-system
-# seal 'kube-system/authelia/authelia-helm-values'
-# seal 'kube-system/authelia/authelia-postgres-helm-values'
-# seal 'kube-system/authelia/authelia-redis-password'
-# seal 'kube-system/dynamic-dns/route53-env'
-# seal 'kube-system/kured/kured-helm-values'
-# seal 'kube-system/openldap/openldap-secrets'
-# seal 'kube-system/registry-creds/registry-creds-secret'
+seal 'kube-system/authelia' 'authelia-helm-values'
+seal 'kube-system/authelia' 'authelia-postgres-helm-values'
+seal 'kube-system/authelia' 'authelia-redis-password'
+seal 'kube-system/dynamic-dns' 'route53-env'
+seal 'kube-system/kured' 'kured-helm-values'
+seal 'kube-system/openldap' 'openldap-secrets'
+seal 'kube-system/registry-creds' 'registry-creds-secret'
 
 # media
 seal 'media/plex' 'plex-secrets'
